0373660f00629a5e4840f24cd22026155c791c4b
Kelley Morales-Shackelford <kelleymoralesshackelford@gmail.com>
2024-02-09 16:36:21 -0500

    thank you card and countdown

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+let cards = (["Guadalupe", "Ollie", "Aki"]);
+let userEvent = "surprise";
+
+function writeCards(cards, userEvent) {
+    let newCards = cards.slice();
+    let newArray = [];
+    for (let i = 0; i < newCards.length; i++) {
+        newArray.push(`Thank you, ${newCards[i]}, for the wonderful ${userEvent} gift!`);       
+    }  
+    return newArray;
+}
+
+let number = 10;
+
+function countDown(number) {
+    let countDown = number;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
